94c111fa5b0419da79ea70ae84667cfd06ad85e7
divinedela.dd <divinedela.dd@gmail.com>
2017-12-21 09:18:01 +0000

    Initial Commit

---
+++ Nunana/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nunana.Startup))]
+namespace Nunana
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
