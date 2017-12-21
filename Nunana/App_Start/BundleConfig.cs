88dce7e8bea9829285004b09ca10b0faae52dfff
divinedela.dd <divinedela.dd@gmail.com>
2017-12-21 18:00:46 +0000

    Add Cerulean Bootswatch theme, style-portfolio theme css for  Home/Index page and add fontawesome.

--- Nunana/App_Start/BundleConfig.cs
+++ Nunana/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Nunana
 {
@@ -24,8 +23,9 @@ namespace Nunana
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+               "~/Content/bootstrap.css",
+               "~/Content/font-awesome.css",
+               "~/Content/stylish-portfolio.css"));
         }
     }
 }
