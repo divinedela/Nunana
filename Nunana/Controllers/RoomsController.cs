cc548df485cf296c2777af0061c40f302e70e795
divinedela.dd <divinedela.dd@gmail.com>
2017-12-21 10:15:23 +0000

    Add Rooms Controller with Index Action and View

---
+++ Nunana/Controllers/RoomsController.cs
@@ -0,0 +1,30 @@
+﻿using Nunana.Models;
+using Nunana.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Nunana.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var rooms = _context.Rooms
+                .Select(a => new RoomsListViewModel
+                {
+                    Id = a.Id,
+                    IsCurrentlyRented = a.IsCurrentlyRented,
+                    RoomNumber = a.RoomNumber,
+                    RoomType = a.Type.ToString()
+                }).ToList();
+
+            return View(rooms);
+        }
+    }
+}
