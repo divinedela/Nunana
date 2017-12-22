ac682d772f3d1e7c298e0fcdffdb2876d8930b79
divinedela.dd <divinedela.dd@gmail.com>
2017-12-22 19:12:26 +0000

    Add Edit Actions and Views for Tenant

--- Nunana/Controllers/TenantsController.cs
+++ Nunana/Controllers/TenantsController.cs
@@ -1,6 +1,7 @@
 ﻿using Nunana.Models;
 using Nunana.ViewModels;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace Nunana.Controllers
@@ -56,6 +57,54 @@ namespace Nunana.Controllers
             };
 
             _context.Tenants.Add(tenant);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var tenant = _context.Tenants.SingleOrDefault(u => u.Id == id);
+            if (tenant == null) return HttpNotFound();
+
+            var tenantViewModel = new TenantFormViewModel
+            {
+                FirstName = tenant.FirstName,
+                LastName = tenant.LastName,
+                Address = tenant.Address,
+                PhoneNumber = tenant.PhoneNumber,
+                Email = tenant.Email,
+                EmergencyContactFirstName = tenant.FirstName,
+                EmergencyContactLastName = tenant.EmergencyContactLastName,
+                EmergencyContactAddress = tenant.EmergencyContactAddress,
+                EmergencyContactEmail = tenant.EmergencyContactEmail,
+                EmergencyContactPhoneNumber = tenant.EmergencyContactPhoneNumber
+            };
+
+            return View(tenantViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TenantFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return View(viewModel);
+
+            var tenant = _context.Tenants.SingleOrDefault(u => u.Id == viewModel.Id);
+            if (tenant == null) return HttpNotFound();
+
+            tenant.FirstName = viewModel.FirstName;
+            tenant.LastName = viewModel.LastName;
+            tenant.Address = viewModel.Address;
+            tenant.PhoneNumber = viewModel.PhoneNumber;
+            tenant.Email = viewModel.Email;
+            tenant.EmergencyContactFirstName = viewModel.FirstName;
+            tenant.EmergencyContactLastName = viewModel.EmergencyContactLastName;
+            tenant.EmergencyContactAddress = viewModel.EmergencyContactAddress;
+            tenant.EmergencyContactEmail = viewModel.EmergencyContactEmail;
+            tenant.EmergencyContactPhoneNumber = viewModel.EmergencyContactPhoneNumber;
+
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
