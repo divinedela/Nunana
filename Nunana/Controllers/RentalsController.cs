1034e6fd62f4d1268d61c33df9c9a737c04afd1a
divinedela.dd <divinedela.dd@gmail.com>
2017-12-23 20:16:09 +0000

    Add Rentals expiring this month Action and View

--- Nunana/Controllers/RentalsController.cs
+++ Nunana/Controllers/RentalsController.cs
@@ -1,5 +1,6 @@
 ﻿using Nunana.Models;
 using Nunana.ViewModels;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -63,6 +64,32 @@ namespace Nunana.Controllers
                 }).ToList();
 
             return View(cancelledRooms);
+        }
+
+        public ActionResult ExpiringThisMonth()
+        {
+            var date = DateTime.Today;
+
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddSeconds(-1);
+
+            var rentals = _context.Rentals
+                .Where(r => !r.IsCancelled)
+                .Where(e => e.StartDate >= firstDayOfMonth && e.EndDate <= lastDayOfMonth)
+                // .Where(e => e.EndDate <= lastDayOfMonth && e.StartDate >= firstDayOfMonth)
+                .Select(a => new RentalListViewModel
+                {
+                    RoomNumber = a.Room.RoomNumber,
+                    TenantName = a.Tenant.FirstName + ", " + a.Tenant.LastName,
+                    RentalStartDate = a.StartDate.ToString(),
+                    RentalEndDate = a.EndDate.ToString(),
+                    RoomId = a.RoomId,
+                    TenantId = a.TenantId,
+                    CreatorName = a.CreatedBy,
+                    NumberOfMonths = DbFunctions.DiffMonths(a.StartDate, a.EndDate).ToString()
+                }).ToList();
+
+            return View(rentals);
         }
     }
 }
