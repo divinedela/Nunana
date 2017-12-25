947513dd8b405e4f8f739d669b984788c5656e63
divinedela.dd <divinedela.dd@gmail.com>
2017-12-25 12:19:21 +0000

    Add Ninject

---
+++ Nunana/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,70 @@
+﻿using Nunana.App_Start;
+using Nunana.Persistence;
+using Nunana.Repositories;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace Nunana.App_Start
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
+    using System;
+    using System.Reflection;
+    using System.Web;
+
+    /// <summary>
+    /// Bootstrapper for the application.
+    /// </summary>
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Load(Assembly.GetExecutingAssembly());
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            kernel.Bind<ITenantRepository>().To<TenantRepository>();
+            kernel.Bind<IRoomRepository>().To<RoomRepository>();
+            kernel.Bind<IRentalRepository>().To<RentalRepository>();
+        }
+    }
+}
